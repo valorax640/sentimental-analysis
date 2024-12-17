d15d9d64124e39ae94e252c0f461cf8e277581e9
Pritam Roy <pritamroy16062003@gmail.com>
2024-12-18 00:41:18 +0530

    Commit

--- python-backend/app.py
+++ python-backend/app.py
@@ -19,6 +19,5 @@ def analyze_sentiment():
 
     return jsonify({"word": word, "sentiment": sentiment, "polarity": polarity})
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
-
+if __name__ == '__main__':
+    app.run(debug=True)
